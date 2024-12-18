1dd567b44698f5fc0b5b18243b15fd8c59347e0d
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-18 17:04:06 +0700

    menambahkan tabell jadwal serta atributnya

--- app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
@@ -1,9 +1,11 @@
 package com.example.ucp2pam.data.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "Dokter")
 data class Dokter(
+    @PrimaryKey
     val id: String,
     val nama: String,
     val spesialis: String,
