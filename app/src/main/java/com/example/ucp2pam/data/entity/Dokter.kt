149551f9b309ae57cd878e498413fb5d8ce7592f
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-18 17:00:25 +0700

    menambah atribut pada dokter

--- app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
@@ -1,6 +1,13 @@
 package com.example.ucp2pam.data.entity
 
+import androidx.room.Entity
+
 @Entity(tableName = "Dokter")
 data class Dokter(
-
+    val id: String,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val noHp: String,
+    val jamKerja: String
 )
