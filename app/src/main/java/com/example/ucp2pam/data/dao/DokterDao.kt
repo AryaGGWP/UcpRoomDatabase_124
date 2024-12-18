9642e9a48adee9e676f37c5abc02c0babb9d7b4b
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-18 17:11:11 +0700

    membuat DokterDaor

---
+++ app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
@@ -0,0 +1,19 @@
+package com.example.ucp2pam.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2pam.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DokterDao {
+    @Insert
+    suspend fun insertDokter(dokter: Dokter)
+
+    @Query ("SELECT * FROM Dokter ORDER BY nama ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
+
+    @Query ("SELECT * FROM Dokter WHERE id = :id")
+    fun getDokter(id:String) : Flow<Dokter>
+}
