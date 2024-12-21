09ef11322189b8d1d32e33c18187cf0babd2a82f
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-22 01:34:26 +0700

    membuat local repository dokter dan customwidget untuk dropdown menu nanti

--- app/src/main/java/com/example/ucp2pam/DependenciesInjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2pam/DependenciesInjection/ContainerApp.kt
@@ -1,2 +1,22 @@
 package com.example.ucp2pam.DependenciesInjection
 
+import android.content.Context
+import com.example.ucp2pam.data.database.AyoSehatDatabase
+import com.example.ucp2pam.repository.LocalRepositoryDkt
+import com.example.ucp2pam.repository.LocalRepositoryJdw
+import com.example.ucp2pam.repository.RepositoryDkt
+import com.example.ucp2pam.repository.RepositoryJdw
+
+interface InterfaceContainerApp{
+    val repositoryDkt: RepositoryDkt
+    val repositoryJdw: RepositoryJdw
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp{
+    override val repositoryDkt: RepositoryDkt by lazy{
+        LocalRepositoryDkt(AyoSehatDatabase.getDatabase(context).dokterDao())
+    }
+    override val repositoryJdw: RepositoryJdw by lazy {
+        LocalRepositoryJdw(AyoSehatDatabase.getDatabase(context).jadwalDao())
+    }
+}
--- app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryDkt.kt
+++ app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryDkt.kt
@@ -1,4 +1,19 @@
 package com.example.ucp2pam.repository
 
-class LocalRepositoryDkt {
+import com.example.ucp2pam.data.dao.DokterDao
+import com.example.ucp2pam.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDkt(private val dokterDao: DokterDao): RepositoryDkt {
+    override suspend fun insertDkt(dokter: Dokter) {
+            dokterDao.insertDokter(dokter)
+        }
+
+    override fun getAllDkt(): Flow<List<Dokter>>{
+        return dokterDao.getAllDokterName()
+    }
+
+    override fun getDkt(dokterId: Int): Flow<Dokter>{
+        return dokterDao.getDokter(dokterId)
+    }
 }
