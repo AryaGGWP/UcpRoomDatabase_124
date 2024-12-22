fe236b9862640ab7d5736da3e6cc62e87db2d663
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-22 21:10:30 +0700

    menambahkan route pada Alamat Navigasi dan membuat widget top app bar

--- app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -1,2 +1,37 @@
 package com.example.ucp2pam.ui.navigation
 
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDokter : AlamatNavigasi {
+    override val route = "dokter"
+}
+
+object DestinasiTambahDokter : AlamatNavigasi {
+    override val route = "tambah_dokter"
+}
+
+object DestinasiJadwal : AlamatNavigasi {
+    override val route = "jadwal"
+}
+
+object DestinasiDetailJadwal : AlamatNavigasi {
+    override val route = "detail_jadwal"
+    const val ID = "idJadwal"
+    val routesWithArg = "$route/{$ID}" // Contoh: detail_jadwal/1
+}
+
+object DestinasiUpdateJadwal : AlamatNavigasi {
+    override val route = "update_jadwal"
+    const val ID = "idJadwal"
+    val routesWithArg = "$route/{$ID}"
+}
+
+object DestinasiTambahJadwal : AlamatNavigasi {
+    override val route = "tambah_jadwal"
+}
