4dd3d850bdf80428a6586099418b794a1b4f4bf4
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-23 18:23:43 +0700

    membuat obejct spesialis

---
+++ app/src/main/java/com/example/ucp2pam/data/Spesialis.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2pam.data
+
+object Spesialis {
+    val option = listOf(
+        "Dokter Umum",
+        "Dokter Persalinan",
+        "Dokter Tulang",
+        "Dokter Mata",
+        "Dokter Jantung",
+        "Dokter Gigi"
+    )
+}
