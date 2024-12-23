434bf19ba899112b2de65077b35a46dfbacd6e66
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-24 04:13:56 +0700

    ADD route navigasi

--- app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -8,30 +8,26 @@ object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
 
-object DestinasiDokter : AlamatNavigasi {
-    override val route = "dokter"
-}
-
 object DestinasiTambahDokter : AlamatNavigasi {
     override val route = "tambah_dokter"
 }
 
-object DestinasiJadwal : AlamatNavigasi {
-    override val route = "jadwal"
+object DestinasiHomeJadwal : AlamatNavigasi {
+    override val route = "home_jadwal"
+}
+
+object DestinasiAddJadwal : AlamatNavigasi {
+    override val route = "add_jadwal"
 }
 
 object DestinasiDetailJadwal : AlamatNavigasi {
     override val route = "detail_jadwal"
-    const val ID = "idJadwal"
-    val routesWithArg = "$route/{$ID}" // Contoh: detail_jadwal/1
+    const val ID = "id"
+    val routesWithArg = "$route/{$ID}"
 }
 
 object DestinasiUpdateJadwal : AlamatNavigasi {
     override val route = "update_jadwal"
-    const val ID = "idJadwal"
+    const val ID = "id"
     val routesWithArg = "$route/{$ID}"
-}
-
-object DestinasiTambahJadwal : AlamatNavigasi {
-    override val route = "tambah_jadwal"
 }
