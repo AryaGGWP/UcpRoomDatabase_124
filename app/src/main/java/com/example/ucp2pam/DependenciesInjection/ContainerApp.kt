eb1ca1d58b93c99d03a5b65ccbf4603dd19b9149
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-24 16:16:57 +0700

    add resource color

--- app/src/main/java/com/example/ucp2pam/DependenciesInjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2pam/DependenciesInjection/ContainerApp.kt
@@ -2,21 +2,16 @@ package com.example.ucp2pam.DependenciesInjection
 
 import android.content.Context
 import com.example.ucp2pam.data.database.AyoSehatDatabase
-import com.example.ucp2pam.repository.LocalRepositoryDkt
-import com.example.ucp2pam.repository.LocalRepositoryJdw
-import com.example.ucp2pam.repository.RepositoryDkt
-import com.example.ucp2pam.repository.RepositoryJdw
+import com.example.ucp2pam.repository.LocalRepositoryApp
+import com.example.ucp2pam.repository.RepositoryApp
 
 interface InterfaceContainerApp{
-    val repositoryDkt: RepositoryDkt
-    val repositoryJdw: RepositoryJdw
+    val repositoryApp: RepositoryApp
 }
 
-class ContainerApp(private val context: Context) : InterfaceContainerApp{
-    override val repositoryDkt: RepositoryDkt by lazy{
-        LocalRepositoryDkt(AyoSehatDatabase.getDatabase(context).dokterDao())
-    }
-    override val repositoryJdw: RepositoryJdw by lazy {
-        LocalRepositoryJdw(AyoSehatDatabase.getDatabase(context).jadwalDao())
+class ContainerApp (private val context: Context) : InterfaceContainerApp {
+    override val repositoryApp: RepositoryApp by lazy{
+        LocalRepositoryApp(AyoSehatDatabase.getDatabase(context).dokterDao(),
+            AyoSehatDatabase.getDatabase(context).jadwalDao())
     }
 }
