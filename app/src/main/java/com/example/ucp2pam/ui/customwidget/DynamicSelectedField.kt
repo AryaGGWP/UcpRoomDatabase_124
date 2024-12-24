1cba23befdcf245a4509ccf02f57b002cd02853e
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-24 16:21:15 +0700

    Add Component untuk dropdown

--- app/src/main/java/com/example/ucp2pam/ui/customwidget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/ucp2pam/ui/customwidget/DynamicSelectedField.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2pam.ui.customwidget
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -14,50 +16,71 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DybnamicSelectedField(
-    value: String,
+fun DynamicSelectedField(
     selectedValue: String,
     options: List<String>,
+    label: String,
+    placeholder: String,
     onValueChangedEvent: (String) -> Unit,
     modifier: Modifier = Modifier
-){
-    var expanded by remember {
-        mutableStateOf(false)
-    }
+) {
+    var expanded by remember { mutableStateOf(false) }
+
     ExposedDropdownMenuBox(
         expanded = expanded,
-        onExpandedChange = {expanded = !expanded},
+        onExpandedChange = {
+            expanded = !expanded },
         modifier = modifier
-    )
-    {
+    ) {
         OutlinedTextField(
             readOnly = true,
             value = selectedValue,
             onValueChange = {},
-            label = {Text(text = "")},
+            label = { Text(text = label,
+                modifier = Modifier,
+                color = Color.Black)},
+            placeholder = { Text(text = placeholder) },
             trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = expanded
+                ExposedDropdownMenuDefaults
+                    .TrailingIcon(
+                        expanded = expanded
                 )
             },
-            colors = OutlinedTextFieldDefaults.colors(),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Black,
+                focusedTextColor = Color.Black,
+                unfocusedTextColor = Color.Black,
+                cursorColor = Color.Black
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            singleLine = true,
+            shape = RectangleShape,
             modifier = Modifier
                 .menuAnchor()
                 .fillMaxWidth()
         )
+
         ExposedDropdownMenu(
             expanded = expanded,
-            onDismissRequest = {expanded = false}
+            onDismissRequest = { expanded = false }
         ) {
-            options.forEach{
-                options: String -> DropdownMenuItem(
-                    text = { Text(text = options) },
+            options.forEach { option: String ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
                     onClick = {
+                        onValueChangedEvent(option)
                         expanded = false
-                        onValueChangedEvent(options)
                     }
                 )
             }
