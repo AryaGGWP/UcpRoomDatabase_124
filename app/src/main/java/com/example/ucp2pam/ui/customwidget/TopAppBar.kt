5ea13bd7c47f178b5655083c689e8889881745e6
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-24 16:17:33 +0700

    add widget top app bar

--- app/src/main/java/com/example/ucp2pam/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2pam/ui/customwidget/TopAppBar.kt
@@ -1,51 +1,142 @@
 package com.example.ucp2pam.ui.customwidget
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2pam.R
 
 @Composable
 fun TopAppBar(
     onBack: () -> Unit,
     showBackButton: Boolean = true,
+    showProfile: Boolean = true,
     judul: String,
+    judulKecil: String,
+    judul2: String,
+    modifier: Modifier = Modifier
 ){
     Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ){
-        if(showBackButton) {
+        modifier = Modifier.fillMaxWidth()
+            .padding(bottom = 16.dp, top = 16.dp)
+    ) {
+        Surface(
+            color = colorResource(id = R.color.topbar),
+            shape = RoundedCornerShape(bottomStart = 15.dp, bottomEnd = 15.dp),
+        ) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp, horizontal = 16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                TextButton(
-                    onClick = onBack,
+                if (showBackButton) {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = colorResource(id = R.color.white)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(
                     modifier = Modifier.align(Alignment.CenterVertically)
                 ) {
-                    Text("Back")
+                    Text(
+                        text = judul,
+                        fontSize = 20.sp,
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(id = R.color.white)
+                    )
+                    Text(
+                        text = judulKecil,
+                        fontSize = 12.sp,
+                        fontStyle = FontStyle.Normal,
+                        fontWeight = FontWeight.Normal,
+                        color = Color.Black
+                    )
                 }
-                Spacer(modifier = Modifier.weight(2f))
+                    if (showProfile) {
+                    Image(
+                        painter = painterResource(id = R.drawable.yoy),
+                        contentDescription = "Profile",
+                        modifier = Modifier
+                            .size(125.dp)
+                            .padding(start = 16.dp)
+                            .clip(shape = CircleShape),
+                        contentScale = ContentScale.FillWidth
+                    )
+                }
             }
         }
-
-        Text(
-            text = judul,
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(Alignment.Center)
-        )
+        ElevatedCard(
+            modifier = Modifier
+                .offset(y = 30.dp)
+                .align(Alignment.BottomCenter),
+            shape = RoundedCornerShape(bottomStart = 30.dp, bottomEnd = 30.dp),
+            elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                { Image(
+                    painter = painterResource(id = R.drawable.yey),
+                    contentDescription = "Icon",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 8.dp)
+                )
+                    Text(
+                    text = judul2,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    color = colorResource(id = R.color.black)
+                )
+                }
+            }
+        }
     }
 }
